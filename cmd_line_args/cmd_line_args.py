f81406bffdd14c4c0bc4f0c5bee27db84ed8ad76
John Brännström <john.brannstrom@gmail.com>
2017-10-10 19:53:01 +0200

    Added the cmd_line_args module.

---
+++ cmd_line_args/cmd_line_args.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. moduleauthor:: John Brännström <john.brannstrom@gmail.com>
+
+Command line script.
+********************
+
+This is a template for writing a script that accepts command line arguments.
+
+"""
+
+# Built in modules
+import argparse
+
+
+class Main():
+    """Contains the script"""
+
+    @staticmethod
+    def _parse_command_line_options():
+        """
+        Parse options from the command line.
+
+        :rtype: Namespace
+        :returns: Command line arguments.
+
+        """
+        debug_help = 'Enter help text for parameter debug here.'  # TODO
+        description = 'Short Description of what the script does.'  # TODO
+        parser = argparse.ArgumentParser(description=description)
+        parser.add_argument('--debug', type=int, default="",
+                            help=debug_help, required=False)
+        args = parser.parse_args()
+        return args
+
+    # noinspection PySimplifyBooleanCheck
+    def run(self):
+        """
+        Run the script.
+        """
+        args = self._parse_command_line_options()
+        print(args.debug)  # TODO delete
+        # TODO enter code here
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
