cd461267d613119e298625f22996b2b37038c978
John Brännström <john.brannstrom@gmail.com>
2020-01-11 11:07:45 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,15 @@
 
 # Copy files to destination with SFTP
 
+# Default path at destnation (can be set with positional argument 2)
+DST_PATH="/tmp/"
+
+# Current location compared to script location if symbolic link
+SYM_DIR="$(dirname "$(readlink -f "$0")")"
+
+# Current location compared to script location
+NO_SYM_DIR="$(dirname $0)"
+
 # SFTP batch file with commands
 SFTP_BATCH_FILE="sftp_deploy_files"
 
@@ -13,10 +22,11 @@ fi
 USER_HOST=${1}
 
 # Check for destination path
-DST_PATH="/path/to/project/"
 if [ "${2}" != "" ]; then
     DST_PATH=${2}
 fi
 
 echo "sftp -b ${SFTP_BATCH_FILE} ${USER_HOST}:${DST_PATH}"
+cd ${SYM_DIR} || return
 sftp -b ${SFTP_BATCH_FILE} ${USER_HOST}:${DST_PATH}
+cd ${NO_SYM_DIR} || return
